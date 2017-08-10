b41ad483dbf829c1ea51105a27309c246464ca77
Brunno Vodola <bvodola@gmail.com>
2017-08-10 13:19:43 -0300

    Dropping the app component on the components folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if(module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default;
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default;
     ReactDOM.render(
       <NextApp />,
       document.getElementById('root')
